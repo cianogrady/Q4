0cb7b1fbce0d229617113fdf09f46b8ae7f81cc3
cianogrady <cianogrady@gmail.com>
2024-07-13 12:00:08 +0000

    ans

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+int main() {
+    
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    // Adding a and b
+    int d = a + b;
+
+    // Print d
+    printf("Value of d: %d\n", d);
+
+    // Subtract c from d
+    int e = c - d;
+
+    // Print e
+    printf("Value of e: %d\n", e);
+}
